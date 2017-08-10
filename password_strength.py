2a1185a9ab63fad72d07abb3492c09c4a0aca62c
DM45 <mda.tomsk@gmail.com>
2017-08-10 22:54:01 +0700

    Решение задачи

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,18 @@
+import string
 
 
 def get_password_strength(password):
-    pass
+    k = 1
+    if len(set(string.digits).intersection(password)) > 0:
+        k += 3
+    if len(set(string.ascii_letters).intersection(password)) > 0:
+        k += 3
+    if len(set("~`!@#$%^&*()_-+={}[]:>;',</?*-+").intersection(password)) > 0:
+        k += 3
+    return k
 
 
 if __name__ == '__main__':
-    pass
+    _password = input('Enter password: ')
+    print('Security level of your password:')
+    print(get_password_strength(_password))
