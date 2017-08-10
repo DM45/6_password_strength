22ab13d440f324c83003d16951a978e647da8b82
DM45 <mda.tomsk@gmail.com>
2017-08-11 00:41:56 +0700

    Исправление ошибок

--- password_strength.py
+++ password_strength.py
@@ -2,15 +2,15 @@ import string
 
 
 def get_password_strength(password):
-    pass_str_lvl = 1
+    pass_stren_lvl = 1
     if len(set(string.digits).intersection(password)):
-        pass_str_lvl += 3
+        pass_stren_lvl += 3
     if (len(set(string.ascii_lowercase).intersection(password)) and
             len(set(string.ascii_uppercase).intersection(password))):
-        pass_str_lvl += 3
+        pass_stren_lvl += 3
     if len(set("~`!@#$%^&*()_-+={}[]:>;',</?*-+").intersection(password)):
-        pass_str_lvl += 3
-    return pass_str_lvl
+        pass_stren_lvl += 3
+    return pass_stren_lvl
 
 
 if __name__ == '__main__':
