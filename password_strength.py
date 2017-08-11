6de047f439f7de42cef40859caf071deb84587d7
DM45 <mda.tomsk@gmail.com>
2017-08-11 18:08:42 +0700

    Исправление ошибки

--- password_strength.py
+++ password_strength.py
@@ -1,19 +1,20 @@
+import getpass
 import string
 
 
 def get_password_strength(password):
-    pass_stren_lvl = 1
+    pass_str_lvl = 1
     if len(set(string.digits).intersection(password)):
-        pass_stren_lvl += 3
+        pass_str_lvl += 3
     if (len(set(string.ascii_lowercase).intersection(password)) and
             len(set(string.ascii_uppercase).intersection(password))):
-        pass_stren_lvl += 3
+        pass_str_lvl += 3
     if len(set("~`!@#$%^&*()_-+={}[]:>;',</?*-+").intersection(password)):
-        pass_stren_lvl += 3
-    return pass_stren_lvl
+        pass_str_lvl += 3
+    return pass_str_lvl
 
 
 if __name__ == '__main__':
-    _password = input('Enter password: ')
+    _password = getpass.getpass('Password:')
     print('Password strength: ')
     print(get_password_strength(_password))
